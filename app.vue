6ca6a2de5d3db864c89609f8066b8605766df64d
JoniLieh <jonasliehmann@gmail.com>
2024-03-03 21:37:04 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- Should be highlight as incorrect -->
+    <h1>{{ mountain?.value?.title }}</h1> 
+
+    <p>{{ mountain?.description }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+// https://nuxt.com/docs/getting-started/data-fetching#client-only-fetching
+const { data: mountain } = useFetch<{
+  title: string;
+  description: string;
+}>('/api/mountains/everest', {
+  pick: [
+    'title', 'description'
+  ]
+})
+</script>
